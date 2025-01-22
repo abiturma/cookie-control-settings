fddec893bc25e0857b2ca7c6e8b6cd905f7b0e18
franz-efang <franzscherer@abicrash.com>
2025-01-22 21:10:29 +0100

    Ich weiß nicht wirklich was ich mache, aber ich versuche den Content-Mode zu aktivieren.

--- analytics-helper.js
+++ analytics-helper.js
@@ -1,16 +1,33 @@
+// Initialisierung der globalen Variablen und Funktionen
 var init = (ids) => {
   window._hjSettings = { hjid: ids.hjId, hjsv: 6 };
   window.dataLayer = window.dataLayer || [];
   window.gtag = function() {
-    window.dataLayer.push(arguments)
-  }
-};
+    window.dataLayer.push(arguments);
+  };
 
+  // Standardmäßige Ablehnung aller Einwilligungen
+  gtag('consent', 'default', {
+    'ad_storage': 'denied',
+    'ad_user_data': 'denied',
+    'ad_personalization': 'denied',
+    'analytics_storage': 'denied'
+  });
+};
+// Funktion zum Akzeptieren von Google Analytics
 var acceptGa = function () {
-  dataLayer.push({
+  gtag('consent', 'update', {
+    'analytics_storage': 'granted',
+    'ad_storage': 'granted',
+    'ad_personalization': 'granted',
+    'ad_user_data': 'granted'
+  });
+
+  window.dataLayer.push({
     'gtm.start': new Date().getTime(),
     event: 'gtm.js'
   });
+
 };
 
 var acceptHj = function () {
